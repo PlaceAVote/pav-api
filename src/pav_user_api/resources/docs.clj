7a333693ada6048b7cfb5fec029a84fae7cf3153
john ervine <johnervine@hotmail.com>
2015-09-15 12:57:26 +0100

    added swagger documentation in the form of yaml and swagger.json

--- src/pav_user_api/resources/docs.clj
+++ src/pav_user_api/resources/docs.clj
@@ -5,21 +5,7 @@
             [pav-user-api.schema.user :refer [User UserRecord]]))
 
 (def user-docs
-  (rs/swagger-json
-    {:info {:title "PAV User API"
-            :description "An API for registering new users and authenticating existing users"
-            :version "0.0.1"}
-     :tags [{:name "user"
-             :description "User actions"}]
-     :paths {"/user" {:put {:summary "Register user"
-                            :description "Register user"
-                            :tags ["user"]
-                            :parameters {:body User}
-                            :responses {201 {:schema UserRecord
-                                             :description "New User Record Created"}}}}}
-     :consumes ["application/json"]
-     :produces ["application/json"]
-     :swagger "2.0"}))
+  (slurp "resources/swagger/swagger.json"))
 
 (defresource swagger-docs
   :available-media-types ["application/json"]
