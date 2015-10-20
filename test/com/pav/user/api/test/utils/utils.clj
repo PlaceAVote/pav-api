ddd259dd14351b6409ec593707151282b18ffc3e
john ervine <johnervine@hotmail.com>
2015-10-20 10:06:14 +0100

    Adding some exception logic around test setup for dynamodb

--- test/com/pav/user/api/test/utils/utils.clj
+++ test/com/pav/user/api/test/utils/utils.clj
@@ -14,16 +14,18 @@
 (def user-table-name (:dynamo-user-table-name env))
 
 (defn delete-user-table []
+  (println "deleting table")
   (try
     (far/delete-table client-opts user-table-name)
-    (catch Exception e (println "Error occured with table setup " e))))
+    (catch Exception e (println "Error occured when deleting table " e " table name: " user-table-name " client-opts " client-opts))))
 
 (defn create-user-table []
+  (println "creating table")
   (try
     (far/create-table client-opts user-table-name [:email :s]
                       {:throughput {:read 5 :write 10}
                        :block? true})
-    (catch Exception e (println "Error occured with table setup " e))))
+    (catch Exception e (println "Error occured with table setup " e " table name: " user-table-name " client-opts " client-opts))))
 
 (defn make-request
   ([method url payload]
