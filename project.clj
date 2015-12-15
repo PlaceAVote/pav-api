51cc754c73851c4229d02eb284103426a4ac8111
johnervine@hotmail.com <johnervine@hotmail.com>
2015-12-15 18:08:23 +0000

    lein-release plugin: bumped version from 0.1.2 to 0.1.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pav-user-api "0.1.2"
+(defproject pav-user-api "0.1.3-SNAPSHOT"
   :description "User API for registering, authenticating and managing user profiles"
   :url ""
   :dependencies [[org.clojure/clojure "1.7.0"]
