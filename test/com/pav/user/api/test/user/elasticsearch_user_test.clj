0288f2e09c56cc82a2ebcc3b184525bedc2750a4
johnervine@hotmail.com <johnervine@hotmail.com>
2015-12-16 08:27:06 +0000

    Some general cleanup around elasticsearch test cases

--- test/com/pav/user/api/test/user/elasticsearch_user_test.clj
+++ test/com/pav/user/api/test/user/elasticsearch_user_test.clj
@@ -1,59 +1,12 @@
 (ns com.pav.user.api.test.user.elasticsearch-user-test
   (:use [midje.sweet])
-  (:require [com.pav.user.api.elasticsearch.user :as eu :refer [index-user]]
+  (:require [com.pav.user.api.elasticsearch.user :as eu :refer [index-user
+																																gather-latest-bills-by-subject]]
             [com.pav.user.api.test.utils.utils :refer [flush-user-index
                                                        bootstrap-bills]]
 						[clojurewerkz.elastisch.query :as q]
-						[clojurewerkz.elastisch.rest :refer [connect]]
-						[clojurewerkz.elastisch.rest.document :as esd]
-						[clojurewerkz.elastisch.rest.response :as esrsp]))
+						[clojurewerkz.elastisch.rest :refer [connect]]))
 
-(def connection (connect))
-
-(defn merge-type-and-fields [hit]
-	(merge {:type (:_type hit)} (:_source hit)))
-
-(defn search-for-term [terms]
-	(when terms
-		(->> (esrsp/hits-from
-					 (esd/search connection "congress" "bill"
-						 :query (q/terms :keywords terms)
-						 :_source [:subject :bill_id :official_title :short_title :popular_title :summary]
-						 :sort  {:updated_at "desc"}))
-			(mapv merge-type-and-fields)))
-	)
-
-(defn to-govtrack-subjects [topic]
-	(case topic
-		"Religion" "religion"
-		"Drugs" "drug"
-		"Defense" "defense"
-		"Politics" "politics"
-		"Gun Rights" "firearms"
-		"Technology" "technology"
-		"Economics" "Economics"
-		"Social Interest" "Social welfare"
-		topic))
-
-(defn to-pav-subjects [subject]
-	(case subject
-		"Arts, culture, religion" "Religion"
-		"Crime and law Enforcement" "Drugs"
-		"Armed forces and national security" "Defense"
-		"Government operations and politics" "Politics"
-		"Crime and law enforcement" "Gun Rights"
-		"Science, technology, communications" "Technology"
-		"Economics and public finance" "Economics"
-		"Social welfare" "Social Interest"
-		subject))
-
-(defn gather-latest-bills-by-subject [topics]
-	(when topics
-		(let [topics (map to-govtrack-subjects topics)
-					bills (->> (search-for-term topics)
-										 flatten
-									   (mapv #(update-in % [:subject] to-pav-subjects)))]
-			bills)))
 
 (against-background [(before :facts (do (flush-user-index)
 																				(bootstrap-bills)))]
