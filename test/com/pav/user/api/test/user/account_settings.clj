81f285617337e5f7e948bd9151ce610827f61726
johnervine@hotmail.com <johnervine@hotmail.com>
2016-01-05 14:29:04 +0000

    Test stub added for account settings endpoint

---
+++ test/com/pav/user/api/test/user/account_settings.clj
@@ -0,0 +1,30 @@
+(ns com.pav.user.api.test.user.account-settings
+	(:use midje.sweet)
+	(:require [com.pav.user.api.test.utils.utils :refer [flush-redis
+																											 flush-dynamo-tables
+																											 flush-user-index]]))
+
+(def test-user {:email "john@stuff.com" :password "stuff2" :first_name "john" :last_name "stuff" :dob "05/10/1984"
+								:country_code "USA" :topics ["Defense"]})
+
+(against-background [(before :facts (do
+																			(flush-dynamo-tables)
+																			(flush-redis)
+																			(flush-user-index)))]
+
+	(fact "Change a public user profile to private"
+		)
+
+	(fact "Update an existing users residence"
+		)
+
+	(fact "Update an existing users Gender"
+		)
+
+	(fact "Update an existing users Email"
+		)
+
+	(fact "Update an existing users DOB"
+		))
+
+
