a68b63110f7ad335bc4835b4aafc07dba4bcd834
johnervine@hotmail.com <johnervine@hotmail.com>
2016-02-05 09:13:13 +0000

    lein-release plugin: preparing 0.1.9 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pav-user-api "0.1.9-SNAPSHOT"
+(defproject pav-user-api "0.1.9"
   :description "User API for registering, authenticating and managing user profiles"
   :url "https://github.com/PlaceAVote/pav-user-api"
   :dependencies [[org.clojure/clojure "1.7.0"]
