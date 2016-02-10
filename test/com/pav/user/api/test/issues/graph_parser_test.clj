15374805f498f5ee686a624f8ab079e4724f251b
johnervine@hotmail.com <johnervine@hotmail.com>
2016-02-10 11:39:07 +0000

    Update open graph parser to include chrome and safari user-agents in header.  Some sites don't provide open graph data for Firefox.

--- test/com/pav/user/api/test/issues/graph_parser_test.clj
+++ test/com/pav/user/api/test/issues/graph_parser_test.clj
@@ -14,3 +14,11 @@
 
 (fact "Given a url, When the resource doesn't contain open graph data, Then return nil"
   (g/extract-open-graph "https://clojuredocs.org/") => nil)
+
+(fact "Given a url, When the site doesn't support Mozilla but Chrome, Then parse open graph data"
+  (let [response (g/extract-open-graph "http://time.com/3319278/isis-isil-twitter/")]
+    response
+    => (contains {:article_link "http://time.com/3319278/isis-isil-twitter/"
+                  :article_title "Why Terrorists Love Twitter"
+                  :article_img "https://timedotcom.files.wordpress.com/2014/09/e9630acbf2ac43c888eac7253e729608-0.jpg?quality=75&strip=color&w=1012"})
+    (some nil? (vals response)) => nil))
