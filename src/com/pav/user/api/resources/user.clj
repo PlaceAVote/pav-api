bd0c90ceb163235b286e3bb4f4285b91b542e7a6
johnervine@hotmail.com <johnervine@hotmail.com>
2016-03-04 11:07:23 +0000

    Removed logging put in place for IP address sourcing

--- src/com/pav/user/api/resources/user.clj
+++ src/com/pav/user/api/resources/user.clj
@@ -82,7 +82,6 @@
   :allowed-methods [:get]
   :available-media-types ["application/json"]
   :exists? (fn [ctx]
-             (log/info (str "request object " (get-in ctx [:request])))
              (if-let [id (retrieve-request-param ctx :user_id)]
                (service/user-profile-exist? (retrieve-token-user-id ctx) id)
                (service/user-profile-exist? (retrieve-token-user-id ctx))))
