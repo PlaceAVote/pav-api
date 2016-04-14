b60f9a461d4eadb36af870d59f82633a2898c865
Sanel Zukan <sanel@placeavote.com>
2016-04-14 14:07:14 +0200

    Script for setting up and starting necessary databases. Not completed yet.

---
+++ bin/start-databases.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+# optionally download and start/stop necessary databases
+
+# Environment variables to setup. If they are not set, script will download
+# databases whose paths aren't set.
+
+WORK_PATH=".db"
+#ES_PATH=""
+#REDIS_PATH=""
+#DYNAMODB_PATH=""
+
+# packages to download with sha1 sums
+es_url="https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.3.1/elasticsearch-2.3.1.tar.gz"
+es_checksum="387c5f045843339d486203b0048ff8911e9c8c54"
+es_dir=$(basename $es_url | sed 's/.tar.gz//')
+
+redis_url="http://download.redis.io/releases/redis-3.0.7.tar.gz"
+redis_checksum="e56b4b7e033ae8dbf311f9191cf6fdf3ae974d1c"
+redis_dir=$(basename $redis_url | sed 's/.tar.gz//')
+
+dynamodb_url="http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz"
+dynamodb_checksum="700811cf99c094eccfcfd8869bb5711906d29f6f"
+dynamodb_dir="dynamodb"
+
+# portable way to figure out path of programs
+find_path() {
+	command -v $1
+}
+
+do_fail() {
+	echo "*** Error: $1"
+	exit 1
+}
+
+trace() {
+	echo "==> " $1
+}
+
+calc_checksum() {
+	if [ -z $(find_path sha1sum) ]; then
+		do_fail "sha1sum tool isn't present."
+	fi
+	
+	sha1sum $1 | awk '{print $1}'
+}
+
+do_download() {
+	downloader=$(find_path "wget")
+	if [ -z $downloader ]; then
+		downloader=$(find_path "curl")
+		if [ -z $downloader ]; then
+			do_fail "unable to find neither 'wget' nor 'curl'. Install one of them first."
+		else
+			# curl -LO will download file as is, following any https requests
+			downloader="$downloader -LO"
+		fi
+	fi
+	
+	url=$1
+	sum=$2
+	archive=$(basename $url)
+	
+	if [ ! -f $archive ]; then
+		trace "Downloading $archive..."
+		$downloader $url
+	fi
+
+	if [ -f $archive ] && [ $sum = $(calc_checksum $archive) ]; then
+		# This is a trick to search for a substring in a string inside bash. There is
+		# also 'perlism' with double squae brackets, but they aren't used here.
+		if [ $archive != ${archive/dynamodb/} ]; then
+			# Amazon does not pack dynamodb in designated folder so we must
+			# do that manually
+			mkdir -p $dynamodb_dir && tar -xpvf $archive -C $dynamodb_dir
+		else
+			tar -xpvf $archive
+			# in case of Redis, compile it too
+			if [ $archive != ${archive/redis/} ]; then
+				if [ -z $(find_path make) ]; then
+					do_fail "unable to find 'make' installed. To build Redis, make sure you have 'make' and C compiler installed."
+				fi
+				trace "Building redis..."
+				(cd $redis_dir && make)
+			fi
+		fi
+	else
+		do_fail "failed to get '$archive' from '$url'."
+	fi
+}
+
+es_start() {
+	trace "Starting elasticsearch..."
+	(cd $ES_PATH && ./bin/elasticsearch 2>../es.err 1>../es.log) &
+}
+
+redis_start() {
+	trace "Starting redis..."
+	echo $(cd $REDIS_PATH && ./src/redis-server 2>../redis.err 1>../redis.log) &
+}
+
+dynamodb_start() {
+	trace "Starting dynamodb..."
+	cmd="java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb"
+	(cd $DYNAMODB_PATH && $cmd  2>../dynamodb.err 1>../dynamodb.log) &
+}
+
+# operate everything in working directory
+mkdir -p $WORK_PATH && cd $WORK_PATH
+
+# check necessary variables and download archives if necessary
+if [ -z $ES_PATH ]; then
+	[ ! -d $es_dir ] && do_download $es_url $es_checksum
+	export ES_PATH=$es_dir
+fi
+
+if [ -z $REDIS_PATH ]; then
+	[ ! -d $redis_dir ] && do_download $redis_url $redis_checksum
+	export REDIS_PATH=$redis_dir
+fi
+
+if [ -z $DYNAMODB_PATH ]; then
+	[ ! -d $dynamodb_dir ] && do_download $dynamodb_url $dynamodb_checksum
+	export DYNAMODB_PATH=$dynamodb_dir
+fi
+
+# start services
+es_start
+redis_start
+dynamodb_start
