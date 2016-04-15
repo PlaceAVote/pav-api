49f9a71b07559fab416fc0f36e4c04aca8db8bdd
Sanel Zukan <sanel@placeavote.com>
2016-04-15 18:35:07 +0200

    Closing #42 - a nasty SSL issue due different Java versions.
    As described in ticket, sunlightfoundation.com has newer certificate,
    not supported in Java 7, causing it to throw
    SSLHandshakeException. Solution is to use http site, which is done on
    Java 6/7; on Java 8 (or other versions, presuming newer ones) it will use
    https site.
    
    This commit also does a small refactoring of location-by-zip function.

--- src/com/pav/api/location/location_service.clj
+++ src/com/pav/api/location/location_service.clj
@@ -6,15 +6,47 @@
 
 (def congress-api-key (:sunlight-congress-apikey env))
 
+(defn- is-java7-or-6?
+  "Returns true if we are running on Java 7 or 6 (7 is first checks, since it is highly a chance we will hit it).
+This is used for SSL workaround for sunlightfoundation.com site."
+  []
+  (let [version (System/getProperty "java.version")]
+     (or (.startsWith version "1.7")
+         (.startsWith version "1.6"))))
+
+(defn- http-to-json
+  "Retrieve url and convert result to JSON. Since Java 7 does not support
+SSL on sunlightfoundation, 'nossl?' parameter should be set to true. 'url' part should be
+without 'http(s)://'."
+  ([url nossl?]
+     (-> (str (if nossl? "http" "https") "://" url)
+         http/get
+         :body
+         (ch/parse-string true)
+         :results
+         first))
+  ([url] (http-to-json url false)))
+
+(defn- to-address-country-code
+  "Take string with address and country code and returns a map with
+:address and :country_code keys, where country code is extracted."
+  [address]
+  (when address
+    (let [address (s/trim address)]
+      {:address address
+       :country_code (-> address (s/split #"\s*,\s*") last)})))
+
 (defn location-by-zip
   "Retrieve location data from googles api and retrieve congressional district from sunlights congress API."
   [zip]
-  (let [search-result (-> (http/get (str "https://maps.googleapis.com/maps/api/geocode/json?address=" zip))
-                        :body (ch/parse-string true) :results first)
+  (let [search-result (http-to-json (str "maps.googleapis.com/maps/api/geocode/json?address=" zip))
         lat-lng (get-in search-result [:geometry :location])
-        district-info (-> (http/get (str "https://congress.api.sunlightfoundation.com/districts/locate?latitude=" (:lat lat-lng) "&longitude=" (:lng lat-lng) "&apikey=" congress-api-key))
-                        :body (ch/parse-string true) :results first)
-        formatted_address (:formatted_address search-result)]
-    (-> {:address formatted_address :country_code (-> (s/split formatted_address #",")
-                                                    last s/trim)}
-      (merge lat-lng district-info))))
+        district-info (http-to-json (format "congress.api.sunlightfoundation.com/districts/locate?latitude=%s&longitude=%s&apikey=%s"
+                                            (:lat lat-lng)
+                                            (:lng lat-lng)
+                                            congress-api-key)
+                                    (is-java7-or-6?))]
+    (merge
+     (-> search-result :formatted_address to-address-country-code)
+     lat-lng
+     district-info)))
