2c4d98f0909308bb7c212d1aee69b2884fb78aaa
johnervine@hotmail.com <johnervine@hotmail.com>
2016-04-18 16:13:24 +0100

    lein-release plugin: bumped version from 0.1.37 to 0.1.38-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pav-user-api "0.1.37"
+(defproject pav-user-api "0.1.38-SNAPSHOT"
   :description "User API for registering, authenticating and managing user profiles"
   :url "https://github.com/PlaceAVote/pav-user-api"
   :dependencies [[org.clojure/clojure "1.8.0"]
