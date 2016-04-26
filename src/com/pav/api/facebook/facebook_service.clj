9b3b5ea35acf9be0bab5b46f1b24979079a40983
johnervine@hotmail.com <johnervine@hotmail.com>
2016-04-26 11:18:43 +0100

    #53 Fix added to use the correct version of facebooks Graph API.  The code was using v2.0 instead of v2.6

--- src/com/pav/api/facebook/facebook_service.clj
+++ src/com/pav/api/facebook/facebook_service.clj
@@ -10,7 +10,7 @@
   facebook-secret (:facebook-client-app-secret env))
 
 (def ^{:doc "Facebook Graph API Endpoint"}
-  graph-api-endpoint "https://graph.facebook.com/oauth")
+  graph-api-endpoint "https://graph.facebook.com/v2.6/oauth")
 
 (def facebook-mode (:facebook-mode env))
 
@@ -20,12 +20,12 @@
   (log/info "Current Users Short Token " short-lived-token)
   (if (= facebook-mode "live")
     (let [response (-> (http/get (str graph-api-endpoint "/access_token")
-                         {:accept :json
-                          :as     :clojure
+                         {:accept       :json
+                          :content-type :json
+                          :as           :auto
                           :query-params {:grant_type    "fb_exchange_token" :client_id facebook-app-id
                                          :client_secret facebook-secret :fb_exchange_token short-lived-token}})
                      :body)]
       (log/info "Facebook Token response " response)
       (or (:access_token response) short-lived-token))
     short-lived-token))
-
