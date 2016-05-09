3fd3f120fdfff1a7a766c7b7057666fd6d256f70
Sanel Zukan <sanel@placeavote.com>
2016-05-10 00:29:35 +0200

    Closes #59: added user persisting.
    Finalizing this ticket where user persisting in both DynamoDB and
    MySQL/H2, with data checks. And there are some tests too...

--- src/com/pav/api/dbwrapper/user.clj
+++ src/com/pav/api/dbwrapper/user.clj
@@ -14,26 +14,6 @@
       (.parse in)
       .getTime))
 
-;  {:user_id 7f55d570-49fc-475e-8c2a-35ad747099f4,
-;   :email test651@placeavote.com,
-;   :password bcrypt+sha512$7be7c879944b55628c8c1d30$12$24326124313224566a6757384e62397a757673384c346964346971534f5868364d626d2f7079567964436853675248367a517331436b775873554f61,
-;   :first_name john,
-;   :last_name stuff,
-;   :dob 05/10/1984,
-;   :country_code USA,
-;   :state CA,
-;   :address Beverly Hills, CA 90210, USA,
-;   :lat 34.1030032,
-;   :lng -118.4104684,
-;   :district 33,
-;   :created_at 1461873506572,
-;   :public true,
-;   :registered nil, :token "xx"
-;   :topics [Defense],
-;   :confirmation-token 2157be72-0afd-416c-839e-0dbc0a71ccfc,
-;   :zipcode 90210,
-;   :gender male}
-
 (defn- convert-user-profile
   "Convert it from dynamodb schema to sql db schema."
   [user-profile]
--- test/com/pav/api/test/dbwrapper/user_test.clj
+++ test/com/pav/api/test/dbwrapper/user_test.clj
@@ -6,13 +6,48 @@
             [com.pav.api.test.utils.utils :as tu]
             [com.pav.api.domain.user :refer [assoc-common-attributes]]
             [com.pav.api.dbwrapper.helpers :refer [with-sql-backend-enabled]]
-            [com.pav.api.dynamodb.db :refer [user-table-name]]))
+            [com.pav.api.dynamodb.db :refer [user-table-name]]
+            [com.pav.api.dynamodb.user :as dynamodb]
+            [com.pav.api.db.user :as sql]))
+
+(defn select-values 
+  "Return values in order for given keys. Removes nil."
+  [mp ks]
+  (remove nil?
+          (reduce #(conj %1 (get mp %2)) [] ks)))
 
 (with-sql-backend-enabled
   (against-background [(before :facts (do (flush-selected-dynamo-tables [user-table-name])
                                           (flush-sql-tables)))]
+
     (fact "Create a new user and validate data in both databases."
       (let [user (assoc-common-attributes (tu/new-pav-user))
-            ret  (u/create-user user)]
-        (map? ret) => true))
+            ret  (u/create-user user)
+            dynamo-ret (dynamodb/get-user-by-id (:user_id ret))
+            sql-ret    (sql/get-user-by-old-id (:user_id ret))]
+        (map? ret) => true
+        (select-values dynamo-ret [:user_id
+                                   :email
+                                   :first_name
+                                   :last_name
+                                   :img_url
+                                   :gender
+                                   :address
+                                   :zipcode
+                                   :state
+                                   :lat
+                                   :lng
+                                   :public]) =>
+        (select-values sql-ret [:old_user_id
+                                :email
+                                :first_name
+                                :last_name
+                                :img_url
+                                :gender
+                                :address
+                                :zipcode
+                                :state
+                                :latitude
+                                :longtitude
+                                :public_profile])))
 ))
