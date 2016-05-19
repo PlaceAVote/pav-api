20eddbb4938a75caca84f2cd2f0c84d2fa8542bf
johnervine@hotmail.com <johnervine@hotmail.com>
2016-05-19 10:16:54 +0100

    #62 refactored vote dao to use new 'sstr' function @sanel is using for performance reasons in other daos.

--- src/com/pav/api/db/vote.clj
+++ src/com/pav/api/db/vote.clj
@@ -1,5 +1,6 @@
 (ns com.pav.api.db.vote
   (:require [com.pav.api.db.db :as db]
+            [com.pav.api.db.tables :refer [user-votes-table]]
             [clojure.java.jdbc :as sql]
             [clojure.tools.logging :as log]
             [com.pav.api.utils.utils :as u]
@@ -9,9 +10,9 @@
 (defn create-user-vote-record [vote]
   (try
     (sql/with-db-transaction [d db/db]
-      (let [id (extract-value (sql/insert! d "user_votes" vote))]
+      (let [id (extract-value (sql/insert! d user-votes-table vote))]
         (log/info "Persisted User Vote to user_votes table" vote)
         id))))
 
 (defn get-user-vote-by-old-id [vote_id]
-  (first (sql/query db/db ["SELECT * from user_votes WHERE old_vote_id=?" vote_id])))
+  (first (sql/query db/db [(u/sstr "SELECT * FROM " user-votes-table " WHERE old_vote_id=?") vote_id])))
