38ad331844d845e6de64554c0852a9a450cec658
johnervine@hotmail.com <johnervine@hotmail.com>
2016-05-20 10:35:22 +0100

    #47 fixed logging error, using wrong id key when retrieving comments from mysql.

--- src/com/pav/api/dbwrapper/from_dynamodb.clj
+++ src/com/pav/api/dbwrapper/from_dynamodb.clj
@@ -30,7 +30,7 @@
   "Copy single bill comment from dynamodb to sql table"
   [comment]
   (if-let [found (sc/get-bill-comment-by-old-id (:comment_id comment))]
-    (log/infof "Skipping comment found using old id '%s'" (:comment_id found))
+    (log/infof "Skipping comment found using old id '%s'" (:id found))
     (try
       (log/infof "Migrating comment '%s'" (:comment_id comment))
       (-> comment dynamodb->sql-comment sc/create-bill-comment)
