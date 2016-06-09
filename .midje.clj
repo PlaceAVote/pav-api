de138129cc3389992b7f62f0458d7cbe30a15b38
Sanel Zukan <sanel@placeavote.com>
2016-06-09 16:06:39 +0200

    Do not use midje any more for junit xml files.

--- .midje.clj
+++
@@ -1,5 +0,0 @@
-;; make sure emitter is set only in CircleCI environment
-(when (System/getenv "CIRCLE_ARTIFACTS")
-  (change-defaults :emitter 'midje.emission.plugins.junit
-                   :print-level :print-facts
-                   :colorize false))
